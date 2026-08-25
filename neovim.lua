109ff006375b6f2ba2f0eb766ed94a1a4f043269
Amirul <muh.amirul.ihsan@gmail.com>
2026-08-26 04:58:00 +0700

    Monokai Pro Light Sun theme with ayu-light backgrounds and accent launcher border

---
+++ neovim.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    priority = 1000,
+    opts = {
+      filter = "light",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
